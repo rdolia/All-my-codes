4903d68e594e633812007443872469a293e4369d
rdolia <43548125+rdolia@users.noreply.github.com>
2018-12-23 14:03:09 +0530

    Add files via upload

---
+++ Assignment5dot3.R
@@ -0,0 +1,19 @@
+#Problem Statement
+#1. Test whether two vectors are exactly equal (element by
+#                                               element)
+vec1 = c(rownames(mtcars[1:15,]))
+vec2 = c(rownames(mtcars[11:25,]))
+
+#2. Sort the character vector in ascending order and descending
+#order
+vec1 = c(rownames(mtcars[1:15,]))
+vec2 = c(rownames(mtcars[11:25,]))
+
+sort(vec1,decreasing = FALSE)
+sort(vec1,decreasing = TRUE)
+
+sort(vec2,decreasing = FALSE)
+sort(vec2,decreasing = TRUE)
+
+#3.What is the major difference between str c() and paste()
+#show an example.
