b1a4bbb00a2a7260cedb97aca3086a846ad3f939
rdolia <43548125+rdolia@users.noreply.github.com>
2018-12-25 23:11:43 +0530

    Add files via upload

--- Assignment6dot1.R
+++ Assignment6dot1.R
@@ -34,13 +34,46 @@ barplot(survived_family_freq$freq)
 #c. Impute the missing values in Age variable using Mice library,
 #create two different graphs showing Age distribution before
 #and after imputation
-
-#Check the data for missing values for column Age
-sapply(titanicdf, function(x) sum(is.na(x)))
-str(titanicdf)
-
 library(mice)
-init = mice(titanicdf, maxit=0) 
-meth = init$method
-predM = init$predictorMatrix
-meth[c(titanicdf$age)]="norm" 
+
+#Removing columns 1,3,8,9,10,12,13,14,15
+
+mini_titanic <- titanicdf[-c(1,3,8,9,10,12,13,14,15)]
+
+md.pattern(mini_titanic)
+
+library(dplyr) 
+mini_titanic <- mini_titanic %>%
+  mutate(
+    survived = as.factor(survived),
+    sex = as.factor(sex),
+    age = as.numeric(age),
+    sibsp = as.factor(sibsp),
+    parch = as.factor(parch),
+    embarked = as.factor(embarked)
+  )
+str(mini_titanic)
+
+
+#running the mice function
+temp_mini_titanic <- mice(mini_titanic,m=5,maxit=50,seed=500)
+summary(temp_mini_titanic)
+
+#check for imputed data in a field
+temp_mini_titanic$imp$age
+
+#Now we can get back the completed dataset using the complete() function.
+completed_mini_titanic <- complete(temp_mini_titanic,1)
+md.pattern(completed_mini_titanic)
+
+#Inspecting the distribution of original and imputed data
+#before imputation - Age 
+
+#The density of the imputed data for each imputed dataset 
+#is showed in magenta while the density of the observed data is showed in blue
+#after imputation of age
+densityplot(temp_mini_titanic)
+#before imputation of age
+densityplot(mini_titanic$age)
+
+
