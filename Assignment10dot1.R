02f80570c4df2cad1a9e906879f9469eaf106e15
rdolia <43548125+rdolia@users.noreply.github.com>
2019-02-21 18:07:32 +0530

    Add files via upload

--- Assignment10dot1.R
+++ Assignment10dot1.R
@@ -78,57 +78,40 @@ qqmath(~AH,data)
 bwplot(~AH,data)
  
 #c. Check for missing values in all columns.
+#Missing values are given value -200. Will replace them with NA
+library(naniar)
+data <- replace_with_na_all(data, condition = ~.x == -200)
+#Visualise missing values
+vis_miss(data)
+#remove columns NMHC.GT. as it as 90 % missing values
+data <- data[-5]
+#remove columns NA. and NA..1 as they have 100% missing values.
+data <- data[-c(15,16)]
+#removing rows 9358 till end as all are NA.
+data <- data[-c(9358:9471),] 
+#visualise again
+vis_miss(data)
 
-table(is.na(data$Date))
-table(is.na(data$Time))
-table(is.na(data$CO.GT.))
-table(is.na(data$PT08.S1.CO.))
-table(is.na(data$NMHC.GT.))
-table(is.na(data$C6H6.GT.))
-table(is.na(data$PT08.S2.NMHC.))
-table(is.na(data$NOx.GT.))
-table(is.na(data$PT08.S3.NOx.))
-table(is.na(data$NO2.GT.))
-table(is.na(data$PT08.S4.NO2.))
-table(is.na(data$PT08.S5.O3.))
-table(is.na(data$T))
-table(is.na(data$RH))
-table(is.na(data$AH))
-table(is.na(data$NA.))
-table(is.na(data$NA..1))
-#We can see that there is NA for all variables in rows 9358 till 9471.
-#Hence we will remove them from data.
-data<-data[-c(9358:9471), ]
-#As last 2 column are full NAs we remove them as well.
-data<- data[,-c(16:17)]
-#Run below commands again to see number of NAs.
-table(is.na(data$Date))
-table(is.na(data$Time))
-table(is.na(data$CO.GT.))
-table(is.na(data$PT08.S1.CO.))
-table(is.na(data$NMHC.GT.))
-table(is.na(data$C6H6.GT.))
-table(is.na(data$PT08.S2.NMHC.))
-table(is.na(data$NOx.GT.))
-table(is.na(data$PT08.S3.NOx.))
-table(is.na(data$NO2.GT.))
-table(is.na(data$PT08.S4.NO2.))
-table(is.na(data$PT08.S5.O3.))
-table(is.na(data$T))
-table(is.na(data$RH))
-table(is.na(data$AH))
-#Impute the missing values using appropriate methods
-#There are no missing values anymore hence no imputation is required.
+# Impute the missing values using appropriate methods
+library(mice)
+md.pattern(data)
+str(data)
+#running the mice function
+data_temp <- mice(data,m=5,maxit=20,seed=500,method = 'cart')
+data_complete <- complete(data_temp,1)
+md.pattern(data_complete)
+
+
 # Create bi-variate analysis for all relationships
 #removing first 2 columns for bi- variate analysis
-data1 <- data[-c(1,2)]
+data1 <- data_complete[-c(1,2)]
 library(corrplot)
-data2 <- as.matrix(data1)
-corrplot(cor(data2))
+
+corrplot(cor(data1))
 #We can see higher correlations between attributes with darker and bigger circles
 
 #Test relevant hypothesis for valid relation
-colnames(data2)
+
 
 cor.test(data1$PT08.S1.CO.,data1$PT08.S2.NMHC.)
 #The p-value of the test is  2.2e-16, which is less than the significance level alpha = 0.05.
@@ -136,3 +119,59 @@ cor.test(data1$PT08.S1.CO.,data1$PT08.S2.NMHC.)
 #correlated with a correlation coefficient of0.9331013.
 #doing the same with blue to dark blue circles in the corrplot will determine 
 #significance of correlation for each attribute pair.
+
+# Create cross tabulations with derived variables
+
+str(data_complete)
+data_complete_derived <- data_complete
+
+data_complete_derived$Year<- format(as.Date(data_complete_derived$Date), "%Y")
+data_complete_derived$Month<- format(as.Date(data_complete_derived$Date), "%m")
+
+
+#cross tabulating to show records in each month. 
+table(data_complete_derived$Month)
+#cross tabulating to show records in each year. 
+table(data_complete_derived$Year)
+
+#check for trends and patterns in time series
+#COGT columm
+data3 <-data_complete[,c(1,3)]
+head(data3)
+#CONVERTING COGT Values to time series
+COGT <- aggregate(CO.GT. ~ Date, data3, mean)
+COGT1 <-ts(COGT$CO.GT.,start = c(2004,3,10),frequency = 365)
+plot(COGT1)
+#We can see by the plot that this column doesnt have any seasonal pattern
+#or variance hence it's stationary.
+
+# PT08.S1.CO.columm
+data4 <-data_complete[,c(1,4)]
+head(data4)
+#computing mean value for each day/date
+PT08.S1.CO <- aggregate(PT08.S1.CO. ~ Date, data4, mean)
+#converting to time series
+PT08.S1.CO1 <-ts(PT08.S1.CO$PT08.S1.CO.,start = c(2004,3,10),frequency = 365)
+#plotting
+plot(PT08.S1.CO1)
+#Again no seasonal or any other pattern for this column.
+
+# C6H6.GT.columm
+data5 <-data_complete[,c(1,5)]
+head(data5)
+#computing mean value for each day/date
+C6H6.GT <- aggregate(C6H6.GT. ~ Date, data5, mean)
+#converting to time series
+C6H6.GT1 <-ts(C6H6.GT$C6H6.GT.,start = c(2004,3,10),frequency = 365)
+#plotting
+plot(C6H6.GT1)
+
+#PT08.S2.NMHC. columm
+data6 <-data_complete[,c(1,6)]
+head(data6)
+#computing mean value for each day/date
+PT08.S2.NMHC <- aggregate(PT08.S2.NMHC. ~ Date, data6, mean)
+#converting to time series
+PT08.S2.NMHC1 <-ts(PT08.S2.NMHC$PT08.S2.NMHC.,start = c(2004,3,10),frequency = 365)
+#plotting
+plot(PT08.S2.NMHC1)
