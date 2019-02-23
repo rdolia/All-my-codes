c07ee95c6f38c0f5b00e1bfee79c332f45cd325d
rdolia <43548125+rdolia@users.noreply.github.com>
2019-02-23 11:00:29 +0530

    Add files via upload

---
+++ SVM.R
@@ -0,0 +1,12 @@
+#svm
+
+data(iris)
+str(iris)
+library(ggplot2)
+qplot(Petal.Length,Petal.Width,data = iris,color=Species)
+install.packages('e1071')
+library(e1071)
+Classifier <- svm(Species~.,data = iris)
+summary(Classifier)
+plot(Classifier,data = iris, Petal.Length~Petal.Width,slice = list(Sepal.Length= 3,Sepal.Width = 3))
+     
