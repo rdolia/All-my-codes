30f5ded6f6d6a981798faf226879a94efaad578b
rdolia <43548125+rdolia@users.noreply.github.com>
2019-03-12 15:20:07 +0530

    Add files via upload

--- SVM.R
+++ SVM.R
@@ -8,5 +8,6 @@ install.packages('e1071')
 library(e1071)
 Classifier <- svm(Species~.,data = iris)
 summary(Classifier)
+
 plot(Classifier,data = iris, Petal.Length~Petal.Width,slice = list(Sepal.Length= 3,Sepal.Width = 3))
      
